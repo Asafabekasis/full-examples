9a279e2d74ec713d8e8dd8a49fce440953971069
asaf.abekasis <asaf.abekasis@brainlab.com>
2024-04-15 13:53:30 +0300

    save form array example

--- client/src/app/examples/formexamples/formarrayexamples/formarrayexamples.component.ts
+++ client/src/app/examples/formexamples/formarrayexamples/formarrayexamples.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-formarrayexamples',
@@ -7,7 +8,7 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./formarrayexamples.component.scss'],
 })
 export class FormarrayexamplesComponent implements OnInit {
-  constructor() {}
+  constructor(public _api: ApiService) {}
 
   public uploadformGroup: FormGroup = new FormGroup({
     products: new FormArray([], [Validators.required], []),
@@ -17,14 +18,39 @@ export class FormarrayexamplesComponent implements OnInit {
     return (this.uploadformGroup.get('products') as FormArray).controls;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this._api.getProducts().subscribe({
+      next: (res) => {
+        let results = res;
+        console.log('results', results);
+        results.forEach((ob) => {
+          this.insertNewProduct(
+            ob.productName,
+            ob.id,
+            ob.color,
+            ob.price,
+            ob.section,
+            ob.active
+          );
+        });
+      },
+      error: (err) => {},
+      complete: () => {
+        console.log('complete');
+      },
+    });
+  }
 
-  insertNewProduct(productName, productId) {
+  insertNewProduct(productName, id, color, price, section, active) {
     this.arrayformControls.push(
       new FormGroup({
         product: this.setProductFormData({
           productName: productName,
-          productId: productId,
+          id: id,
+          color: color,
+          price: price,
+          section: section,
+          active: active,
         }),
       })
     );
@@ -32,18 +58,22 @@ export class FormarrayexamplesComponent implements OnInit {
     console.log(this.arrayformControls);
   }
 
-  productClicked(product, e, i) {
-    console.log(product);
-
+  productClicked( e, i) {
+    console.log(e);
     console.log(i);
+    
     this.arrayformControls[i].get('product.productName').setValue('papapapa');
   }
 
-  setProductFormData({ productName, productId }) {
+  setProductFormData({ productName, id, color, price, section, active }) {
     return new FormGroup(
       {
         productName: new FormControl(productName, [], []),
-        ProductId: new FormControl(productId, [], []),
+        id: new FormControl(id, [], []),
+        color:new FormControl(color, [], []),
+        price: new FormControl(price, [], []),
+        section:new FormControl(section, [], []),
+        active:new FormControl(active, [], [])
       },
       [],
       []
